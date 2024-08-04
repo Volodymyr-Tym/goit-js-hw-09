c0ba3498e03df77fd8ccabf9245c45314d8a3d5d
Volodymyr-Tym <163361581+Volodymyr-Tym@users.noreply.github.com>
2024-08-04 23:47:49 +0000

    Deploying to gh-pages from @ Volodymyr-Tym/goit-js-hw-09@1e67277437f4285535e1eb6664368650e8e7a02c 🚀

--- commonHelpers2.js
+++ commonHelpers2.js
@@ -1,2 +1,2 @@
-import"./assets/modulepreload-polyfill-3cfb730f.js";document.querySelector(".feedback-form");
+import"./assets/modulepreload-polyfill-3cfb730f.js";const a={email:"",message:""},e=document.querySelector(".feedback-form"),r=t=>{const l=t.target.name;let o=t.target.value;l==="email"?a.email=o.trim():a.message=o.trim();const m=JSON.stringify(a);localStorage.setItem("feedback-form-state",m)},s=t=>{t.preventDefault(),e.email.value.trim()===""||e.message.value.trim()===""?alert("Fill please all fields"):(console.log(a),localStorage.removeItem("feedback-form-state"),e.reset())};e.addEventListener("input",r);e.addEventListener("submit",s);
 //# sourceMappingURL=commonHelpers2.js.map
