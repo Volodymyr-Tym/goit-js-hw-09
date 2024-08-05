216824b517de8579d3008a67c644631695a5b1e3
Vova Tym <v.tymoshyshyn@gmail.com>
2024-08-05 23:24:07 +0100

    Make task 2

--- src/js/2-form.js
+++ src/js/2-form.js
@@ -1,12 +1,45 @@
-const formData = {
+let formData = {
   email: '',
   message: '',
 };
 const form = document.querySelector('.feedback-form');
 
+const loadDataFromLS = key => {
+  try {
+    const formDataFromLS = localStorage.getItem(key);
+
+    if (formDataFromLS !== null) {
+      const parsedData = JSON.parse(formDataFromLS);
+      formData = parsedData;
+      return parsedData;
+    }
+  } catch (err) {
+    console.log(err);
+  }
+};
+
+const saveDataToLS = (key, value) => {
+  try {
+    localStorage.setItem(key, JSON.stringify(value));
+  } catch (err) {
+    console.log(err);
+  }
+};
+
+const fillFormFieldsFromLS = () => {
+  const dataFromLS = loadDataFromLS('feedback-form-state');
+
+  if (dataFromLS !== undefined) {
+    form.email.value = dataFromLS.email;
+    form.message.value = dataFromLS.message;
+
+    return;
+  }
+};
+
 const onFormFieldInput = event => {
   const fieldName = event.target.name;
-  let fieldValue = event.target.value;
+  const fieldValue = event.target.value;
 
   if (fieldName === 'email') {
     formData.email = fieldValue.trim();
@@ -14,23 +47,24 @@ const onFormFieldInput = event => {
     formData.message = fieldValue.trim();
   }
 
-  const formDataToLS = JSON.stringify(formData);
-  localStorage.setItem('feedback-form-state', formDataToLS);
+  saveDataToLS('feedback-form-state', formData);
 };
 
 const onFormSubmit = event => {
   event.preventDefault();
+
   if (form.email.value.trim() === '' || form.message.value.trim() === '') {
     alert('Fill please all fields');
   } else {
     console.log(formData);
+    formData.email = '';
+    formData.message = '';
     localStorage.removeItem('feedback-form-state');
-    form.reset();
+    event.target.reset();
   }
 };
 
-// const formDataFromLS = localStorage.getItem('feedback-form-state');
-// console.log(JSON.parse(formDataFromLS));
-
 form.addEventListener('input', onFormFieldInput);
 form.addEventListener('submit', onFormSubmit);
+
+fillFormFieldsFromLS();
